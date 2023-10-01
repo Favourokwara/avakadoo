65b7f8cba9e414e6c68542d9da8e6b4f44b29af2
maanton022 <maanton022@student.wethinkcode.co.za>
2023-10-01 02:15:01 +0200

    feat: add dashboard links

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,17 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
+import { SetStateAction, useState } from "react";
 
 export default function Dashboard() {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState("profile"); // Initial tab
+
+  // Function to switch the active tab and update the route
+  const switchTab = (tab: SetStateAction<string>) => {
+    setActiveTab(tab);
+    router.push(`/dashboard/${tab}`);
+  };
+
   return (
     <div className="flex min-h-screen">
       {/* Navigation Bar */}
@@ -8,13 +19,27 @@ export default function Dashboard() {
         <h1 className="text-white text-xl font-semibold">Doctor's Orders</h1>
         <ul className="mt-8">
           <li>
-            <Link href="/dashboard/profile text-white hover:text-[#2e026d]" passHref>
-              <a className="text-white hover:text-[#2e026d]">Profile</a>
+            <Link href="/dashboard/profile" passHref>
+              <a
+                onClick={() => switchTab("profile")}
+                className={`text-white hover:text-[#2e026d] ${
+                  activeTab === "profile" ? "font-bold" : ""
+                }`}
+              >
+                Profile
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/dashboard/appointments" passHref>
-              <a className="text-white hover:text-[#2e026d]">Appointments</a>
+              <a
+                onClick={() => switchTab("appointments")}
+                className={`text-white hover:text-[#2e026d] ${
+                  activeTab === "appointments" ? "font-bold" : ""
+                }`}
+              >
+                Appointments
+              </a>
             </Link>
           </li>
           {/* Add more navigation links as needed */}
@@ -22,8 +47,18 @@ export default function Dashboard() {
       </nav>
       {/* Main Content */}
       <main className="flex-grow p-4">
-        {/* Your dashboard content goes here */}
+        {activeTab === "profile" && <ProfileContent />}
+        {activeTab === "appointments" && <AppointmentsContent />}
+        {/* Add more tab content as needed */}
       </main>
     </div>
   );
+}
+
+function ProfileContent() {
+  return <div>This is the Profile content.</div>;
+}
+
+function AppointmentsContent() {
+  return <div>This is the Appointments content.</div>;
 }
